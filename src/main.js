2eb7d2f86ef36a9a8de879bd4c63066d2442e6b1
fila90 <ivan.filipovic.fila@gmail.com>
2023-06-15 19:03:01 +0200

    add current rates, fix favicon, fix dates

--- src/main.js
+++ src/main.js
@@ -2,16 +2,20 @@ import Chart from "chart.js/auto";
 import { format, parseISO } from "date-fns";
 
 let myChart;
+const rateErste = document.querySelector(".rate--erste");
+const rateUcb = document.querySelector(".rate--ucb");
 
 fetch("/api/exchange")
   .then((resp) => resp.json())
-  .then((resp) => {
-    console.log(resp);
-    generateChart(resp);
-  })
+  .then((resp) => generateChart(resp))
   .catch((err) => console.error(err));
 
 function generateChart(data) {
+  const { erste = [], ucb = [] } = data;
+
+  rateErste.innerText = `Erste ~ ${erste.at(-1).rate}`;
+  rateUcb.innerText = `UCB ~ ${ucb.at(-1).rate}`;
+
   if (myChart) {
     myChart.destroy();
   }
@@ -19,23 +23,19 @@ function generateChart(data) {
   myChart = new Chart(document.querySelector("#chart"), {
     type: "line",
     data: {
-      labels: data.erste.map((rate) =>
-        format(parseISO(rate.date), "dd/MM/yyyy")
-      ),
+      labels: data.erste.map((rate) => format(parseISO(rate.date), "dd/MM")),
       datasets: [
         {
           backgroundColor: "rgba(40,112,237,.7)",
           borderColor: "rgba(40,112,237,.7)",
-          data: (data.erste ?? []).map((rate) => rate.rate),
+          data: erste.map((rate) => rate.rate),
           label: "Erste",
-          tension: 0.1,
         },
         {
           backgroundColor: "#e2001a",
           borderColor: "#e2001a",
-          data: (data.ucb ?? []).map((rate) => rate.rate),
+          data: ucb.map((rate) => rate.rate),
           label: "UCB",
-          tension: 0.1,
         },
       ],
     },
